adbcc480de07d4fd997ff25bbe2d3b27ba15aaed
Narek <sayadyannarek@gmail.com>
2016-11-10 22:37:03 +0400

    add test

---
+++ src/test/java/Test.java
@@ -0,0 +1,23 @@
+import common.universityexception.UniversityAppException;
+import controller.RegisterController;
+
+/**
+ * Created by Narek on 10.11.2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        test.register("testik1", "passtest1");
+
+    }
+    public void register(String u, String p) {
+        RegisterController registerController = new RegisterController();
+        try {
+            registerController.registerStudent(u, p);
+        } catch (UniversityAppException e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+}
