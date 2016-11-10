2b574a1bfe0fa49fb316fd685660c405f02f99ee
Narek <sayadyannarek@gmail.com>
2016-11-10 22:02:51 +0400

    changes in model, controller and repository of Student.

---
+++ src/main/java/repository/interfaces/CRUD.java
@@ -0,0 +1,11 @@
+package repository.interfaces;
+
+/**
+ * Created by Narek on 09.11.2016.
+ */
+public interface CRUD<T> {
+    T create(T obj);
+    T read(T obj);
+    T update(T obj);
+    void delete(T obj);
+}
